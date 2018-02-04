0c7319e42fcb77aa2b376656c34900ed6b391427
Federico Bustos Fierro <federico0812@gmail.com>
2018-02-04 02:16:48 -0300

    todo

---
+++ AppDelegateObjective.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegateObjective.h
+//  EventosSirioLibanes
+//
+//  Created by Federico Bustos Fierro on 2/4/18.
+//  Copyright © 2018 Federico Bustos Fierro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegateObjective : UIApplication
+
+@end
